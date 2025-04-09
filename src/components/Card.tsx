27d732ec30aabffd391cc107f91ba4e78d70f273
Nicole <76132106+nicolecomputer@users.noreply.github.com>
2025-04-09 11:17:41 -0600

    Allow flipping of cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,17 +3,24 @@ import "./card.css"
 
 type CardProps = {
     symbol: string,
-    isFaceUp: boolean
+    isFaceUp: boolean,
+    onClick: () => void
 }
 
-export default function Card({ symbol, isFaceUp }: CardProps) {
+export default function Card({ symbol, isFaceUp, onClick }: CardProps) {
     const flippedClass = isFaceUp ? "face-up-card" : "face-down-card"
 
     return (
-        <button className={toClasslist([
-            "card",
-            flippedClass
-        ])}>
+        <button
+            className={toClasslist([
+                "card",
+                flippedClass
+            ])}
+            onClick={(event) => {
+                event.stopPropagation()
+                onClick()
+            }}
+        >
             <p>{isFaceUp ? symbol : ""}</p>
         </button>
     )
--- src/components/CardBoard.tsx
+++ src/components/CardBoard.tsx
@@ -1,31 +1,44 @@
 import "./cardboard.css"
 import Card from "./Card";
+import React from "react";
+
+type Card = {
+    id: string,
+    symbol: string
+}
 
 export default function CardBoard() {
-    const symbols = [
-        "👩‍💻",
-        "👩‍💻",
-        "🎂",
-        "🎂",
-        "💝",
-        "💝",
-        "💤",
-        "💤",
-        "🧶",
-        "🧶",
-        "🥸",
-        "🥸",
-        "🦀",
-        "🦀",
-        "🐸",
-        "🐸",
+    const symbols: Card[] = [
+        { symbol: "👩‍💻", id: "1" },
+        { symbol: "👩‍💻", id: "2" },
+        { symbol: "🎂", id: "3" },
+        { symbol: "🎂", id: "4" },
+        { symbol: "💝", id: "5" },
+        { symbol: "💝", id: "6" },
+        { symbol: "💤", id: "7" },
+        { symbol: "💤", id: "8" },
+        { symbol: "🧶", id: "9" },
+        { symbol: "🧶", id: "10" },
+        { symbol: "🥸", id: "11" },
+        { symbol: "🥸", id: "12" },
+        { symbol: "🦀", id: "13" },
+        { symbol: "🦀", id: "14" },
+        { symbol: "🐸", id: "15" },
+        { symbol: "🐸", id: "16" },
     ]
+
+    const [flippedCardId, setFlippedCardId] = React.useState<string | null>(null)
+
     return (
         <div className='card-board'>
-            {symbols.map((symbol, index) =>
+            {symbols.map((card) =>
                 <Card
-                    key={`card-${index}`} symbol={symbol}
-                    isFaceUp={false}
+                    key={`card-${card.id}`}
+                    symbol={card.symbol}
+                    isFaceUp={flippedCardId === card.id}
+                    onClick={() => {
+                        setFlippedCardId(card.id)
+                    }}
                 />
             )}
         </div>
