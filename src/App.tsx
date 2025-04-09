17f9d46a17af11aab7feecb8a98c6aaefaca60aa
Nicole <76132106+nicolecomputer@users.noreply.github.com>
2025-04-09 11:55:54 -0600

    Evaluate win conditioon

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,47 @@ import './App.css'
 import CardBoard from './components/CardBoard'
 import { CardId } from './lib/match-game'
 
+type GameState = "playing" | "won"
+
 function App() {
+  const cards = [
+    { symbol: "👩‍💻", id: "1" },
+    { symbol: "👩‍💻", id: "2" },
+    { symbol: "🎂", id: "3" },
+    { symbol: "🎂", id: "4" },
+    { symbol: "💝", id: "5" },
+    { symbol: "💝", id: "6" },
+    { symbol: "💤", id: "7" },
+    { symbol: "💤", id: "8" },
+    { symbol: "🧶", id: "9" },
+    { symbol: "🧶", id: "10" },
+    { symbol: "🥸", id: "11" },
+    { symbol: "🥸", id: "12" },
+    { symbol: "🦀", id: "13" },
+    { symbol: "🦀", id: "14" },
+    { symbol: "🐸", id: "15" },
+    { symbol: "🐸", id: "16" },
+  ]
   const [matchedCardIds, setMatchedCardIds] = React.useState<CardId[]>([])
+  const [gameState, setGameState] = React.useState<GameState>("playing")
   return (
     <>
       <h1>Memory Game</h1>
       <CardBoard
         matchedCardIds={matchedCardIds}
-        cards={[
-          { symbol: "👩‍💻", id: "1" },
-          { symbol: "👩‍💻", id: "2" },
-          { symbol: "🎂", id: "3" },
-          { symbol: "🎂", id: "4" },
-          { symbol: "💝", id: "5" },
-          { symbol: "💝", id: "6" },
-          { symbol: "💤", id: "7" },
-          { symbol: "💤", id: "8" },
-          { symbol: "🧶", id: "9" },
-          { symbol: "🧶", id: "10" },
-          { symbol: "🥸", id: "11" },
-          { symbol: "🥸", id: "12" },
-          { symbol: "🦀", id: "13" },
-          { symbol: "🦀", id: "14" },
-          { symbol: "🐸", id: "15" },
-          { symbol: "🐸", id: "16" },
-        ]}
+        cards={cards}
         onMatch={(cardIds) => {
-          setMatchedCardIds([
+          const nextMatches = [
             ...matchedCardIds,
             ...cardIds
-          ])
+          ]
+          setMatchedCardIds(nextMatches)
+          if (nextMatches.length === cards.length) {
+            setGameState("won")
+          }
         }}
       />
+      <p>Game state: {gameState}</p>
     </>
   )
 }
